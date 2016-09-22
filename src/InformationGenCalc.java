eb35a31347dfc12e608d8291f4d4f33c4cf37046
Leogaogithub <longhaogao@gmail.com>
2016-09-22 13:08:52 -0500

    implement decision tree

---
+++ src/InformationGenCalc.java
@@ -0,0 +1,80 @@
+import java.util.List;
+
+
+public class InformationGenCalc {
+	int attriNum = 0;
+	void setAttriNum(int attriNum ){
+		this.attriNum = attriNum;
+	}
+	
+	InformationGenCalc(int attriNum ){
+		this.attriNum = attriNum;
+	}
+	
+	public int counts(List<Integer[]> examples, int colum, int target){
+		int count = 0;
+		for(Integer[] e : examples){
+			if(target==e[colum]) count++;
+		}
+		return count;
+	}
+	
+	private int countsOnCondition(List<Integer[]> examples, int conditionColum, int conditionValue, int colum, int target){
+		int count = 0;
+		for(Integer[] e : examples){
+			if(e[conditionColum]==conditionValue && target==e[colum]) count++;
+		}
+		return count;
+	}
+	
+	int getBestClassifies(List<Integer[]> examples, List<Integer> attributes){
+		int bestAttr = 0;
+		double max = Integer.MIN_VALUE;
+		for(Integer attr: attributes){
+			double cur = informationGen(examples, attr);
+			if(max < cur){
+				max = cur;
+				bestAttr = attr;
+			}
+		}
+		return bestAttr;
+	}
+	
+	private double entropy(int pos, int neg){
+		double result = 0;
+		double total = pos + neg;
+		double probability = pos/total;
+		if(pos!=0){
+			result -= probability*(Math.log10(probability)/Math.log10(2.0));
+		}
+		if(neg!=0){
+			result -= (1-probability)*(Math.log10( 1 - probability)/Math.log10(2.0));
+		}
+		return result;
+	}
+	
+	double informationGen(List<Integer[]> examples, int attributes){
+		double result = 0;
+		int total = examples.size();
+		// calculate entropy of root
+		int yPos = counts(examples, attriNum-1, 1);
+		int yNeg = total - yPos;
+		double entropySef = entropy(yPos,yNeg);
+		int totalLeft = counts(examples, attributes, 0);
+		int totalRight = total - totalLeft;
+		// calculate entropy of left child
+		int yPosLeft = countsOnCondition(examples,attributes, 0, attriNum-1, 1);
+		int yNegLeft = totalLeft - yPosLeft;
+		double entropyLeft = entropy(yPosLeft,yNegLeft);
+		// calculate entropy of right child
+		int yPosRight = countsOnCondition(examples,attributes, 1, attriNum-1, 1);
+		int yNegRight = totalRight - yPosRight;
+		double entropyRight = entropy(yPosRight,yNegRight);
+		
+		result = entropySef;
+		result -= (totalLeft/(double)total)*entropyLeft;
+		result -= (totalRight/(double)total)*entropyRight;
+		
+		return result;
+	}
+}
---
+++ src/UtilityTool.java
@@ -0,0 +1,80 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author leo 
+ *  email: longhaogao@gmail.com
+ *
+ * @date Apr 12, 2016
+ * @version 1.0
+ * 
+ * @date June 8, 2016
+ * @version 1.1
+ */
+
+public class UtilityTool {
+	static String oneBlank = " ";
+	static String twoBlanks = "  ";
+	
+	public static String getWordsAfter(String str, String where){
+		int startIdx = str.indexOf(where);
+		int endIdx = str.length();
+		String midStr = str.substring(startIdx+where.length(), endIdx);
+		return midStr.trim();
+	}
+	
+
+	public static String getMatcher(String regex, String source) {  
+        String result = "";  
+        Pattern pattern = Pattern.compile(regex);  
+        Matcher matcher = pattern.matcher(source);  
+        while (matcher.find()) {  
+            result = matcher.group(1); 
+        }  
+        return result;  
+    } 
+	
+	public static boolean checkStringNotEmpty(String item){
+		if(item==null)
+			return false;
+		if(item.isEmpty())
+			return false;
+		if(item.equals(""))
+			return false;
+		return true;
+	}	
+
+	 
+	public static String regularingLine(String str){		
+
+		str = str.replaceAll("\n", oneBlank);
+		str = str.replaceAll("\t", oneBlank);		
+		if(str.contains("#")){
+			str = str.substring(0, str.indexOf("#"));
+		}
+		//remove extral blank
+		str = removeExtralBlank(str);		
+		return str; 
+	}
+	
+	
+	public static String removeExtralBlank(String str){
+		str = str.trim();
+		while(str.contains(twoBlanks)){
+			str = str.replaceAll(twoBlanks,oneBlank);
+		}		
+		return str;
+	}
+	
+	
+	public  static void line(String cont){
+		System.out.println(cont);
+	}
+	
+	public static boolean isValidLine(String str){		
+		str = str.trim();
+		if(!checkStringNotEmpty(str)) return false;		
+		if(str.startsWith("#")) return false;
+		return true;		
+	}		     
+}
